d54645d743aea0ec216466bd59cb3a0dfd7b8c02
winkerVSbecks <varunvachhar@gmail.com>
2025-06-04 17:06:59 -0400

    fix a11y issues

--- packages/react/src/components/Button/Button.stories.js
+++ packages/react/src/components/Button/Button.stories.js
@@ -58,13 +58,6 @@ export default {
       page: mdx,
     },
   },
-  decorators: [
-    (Story) => (
-      <div style={{ '--cds-text-on-color': '#aaa' }}>
-        <Story />
-      </div>
-    ),
-  ],
 };
 
 export const Default = (args) => {
